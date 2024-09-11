58abe329f75545e19689a8e53240775812583a3e
dovillegas <dario.oscar.villegas@comunidad.unne.edu.ar>
2024-09-11 17:05:24 -0300

    Actualización

---
+++ script/script_ddl_proyecto.sql
@@ -0,0 +1,6 @@
+-- SCRIPT TEMA "nombre del tema"
+-- DEFINNICIÓN DEL MODELO DE DATOS
+
+CREATE DATATABLE nombre_base;
+
+USE nombre_base;
---
+++ script/script_insert_proyecto.sql
@@ -0,0 +1,6 @@
+-- SCRIPT "nombre del proyecto"
+-- INSERCIÓN DEL LOTE DE DATOS
+
+INSERT INTO table_a VALUES (0,1);
+INSERT INTO table_a VALUES (1,2);
+
