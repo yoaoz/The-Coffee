f54b490c96a9b486d14e5d39edd2eecf8ae5883d
ClarisaK <118145755+ClarisaK@users.noreply.github.com>
2024-11-12 21:38:12 -0300

    Agregué tema 3 de optimización de consultas a través de índices

---
+++ script/Tema_03_Optimización_de_consultas_a_través_de_índices/Optimización_de_consultas_a_través_de_índices.md
@@ -0,0 +1,36 @@
+﻿# Tema: Optimización de consultas a través de índices
+## Introduccion
+
+La optimización de las consultas a través de la utilización de índices es un método esencial en las bases de datos para incrementar el desempeño y la rapidez de la búsqueda, particularmente en consultas complejas o en tablas extensas. 
+
+## ¿Qué es un índice?
+
+Un índice es una estructura de datos adicional que se crea en una tabla para mejorar la velocidad de las consultas, su propósito es encontrar una manera de disminuir el tiempo de respuesta de la consulta, evitando así el consumo excesivo de recursos, recuperar datos más rápido y reducir cuellos de botella. Permite a la base de datos localizar rápidamente las filas que cumplen con los criterios de búsqueda y sirven como técnica de optimización del rendimiento.
+
+## Tipos de índice y sus aplicaciones
+
+* **Índices agrupados (Clustered Index):** 
+	Los índices agrupados organizan y guardan las filas de información en la tabla o vista basándose en sus valores claves, define el orden en el cual los datos son físicamente almacenados en una tabla.
+En una tabla Agrupada, se utiliza un índice agrupado SQL Server para guardar las filas de información categorizadas en función de los valores clave del índice agrupado. SQL Server nos ofrece la posibilidad de crear únicamente un índice agrupado por cada tabla, dado que los datos pueden ser categorizados en la tabla mediante un criterio de orden.
+Guarda los datos de la tabla en el disco duro en la secuencia del índice. Solo puede existir un índice organizado por tabla, dado que establece la secuencia física de los registros.
+El índice agrupado se organiza mediante estructuras B-Árbol, con el objetivo de activar el Motor SQL Server para localizar rápidamente las filas necesarias vinculadas a los valores clave de índice. Se percibe cada página en la estructura de índice B-Árbol como un tipo de índice. El nodo superior se conoce como nodo Raíz y los nodos inferiores como nodos Hoja, donde se guardan y categorizan las páginas de las tablas de información según los valores clave de índice.
+En las tablas montón (que son aquellas tablas que no tienen un índice agrupado), la ausencia de los índices agrupados significa que la información no es clasificada en la tabla que le sigue.
+Ideal para columnas que se emplean a menudo en rangos de búsqueda o en columnas singulares, como una fecha o un identificador singular.
+-- **Ejemplo**
+`CREATE CLUSTERED INDEX IDX_Movimiento_Caja_Fecha ON Movimiento_Caja(fecha);`
+
+* **Índices  no agrupados (Non-Clustered Index):** 
+Un índice no agrupado no ordena los datos físicos dentro de la tabla. De hecho, un índice no agrupado es agrupado en un solo lugar y los datos de la tabla son almacenados en otro lugar. 
+Dentro de la tabla los datos serán ordenados por un índice agrupado. De todos modos, por dentro los datos del índice no agrupado son almacenados en un orden específico. El índice contiene valores de columna en los cuales el índice es creado y la dirección del registro a la que el valor de la columna pertenece.
+Cuando una consulta es lanzada contra una columna en la cual el índice es creado, la base de datos primero irá al índice y buscará la dirección de la fila correspondiente en la tabla. Luego, irá a esa dirección de fila y obtendrá otros valores de columna. Es debido a este paso adicional que los índices no agrupados son más lentos que los índices agrupados.
+Mantiene una estructura separada, donde se almacena el valor de la clave y un puntero a la fila correspondiente. Esto permite múltiples índices no agrupados en una misma tabla, lo que mejora la flexibilidad para optimizar diferentes consultas.
+Útil en columnas que no son clave primaria, pero que se consultan con frecuencia, como columnas en filtros WHERE.
+Mejora el rendimiento de búsquedas y consultas en columnas específicas, pero no impacta el orden físico de los datos.	
+--**Ejemplo**
+`CREATE NONCLUSTERED INDEX IX_Movimiento_Caja_Fecha_TipoMovimiento_Include ON Movimiento_Caja (fecha, tipo_movimiento) INCLUDE (monto, descripcion);`
+
+## Conclusiones
+
+Es esencial el uso de índices en las bases de datos para optimizar la eficacia de las consultas, particularmente en tablas con grandes cantidades de información y en consultas complejas. Los índices facilitan que la base de datos pueda acceder con rapidez a las filas que satisfacen los criterios de búsqueda, reduciendo así el tiempo de respuesta y el uso de recursos, lo que disminuye los atascos y acelera el proceso.
+Los índices agrupados son ideales para columnas que se consultan en intervalos determinados y que establecen la secuencia lógica de la tabla, como fechas o identificadores únicos. Al guardar los datos en una secuencia determinada, el acceso resulta más veloz. No obstante, solo se puede tener un índice organizado por tabla, ya que establece la secuencia física de los datos. Al estar directamente ligado a las filas de datos, el índice agrupado no requiere espacio adicional más allá de los datos en sí.
+Los índices no agrupados resultan beneficiosos para optimizar el desempeño de las consultas en columnas concretas que no establecen la secuencia de la tabla, pero se realizan a menudo. A pesar de que son más lentos que los índices agrupados por la búsqueda de un puntero extra, facilitan el acceso a columnas adicionales y proporcionan flexibilidad al albergar varios índices en la misma tabla. Requiere espacio adicional en disco, ya que los datos del índice y los datos de la tabla están separados.
---
+++ script/Tema_03_Optimización_de_consultas_a_través_de_índices/script.sql
@@ -0,0 +1,156 @@
+USE DBTheCoffee;
+
+-- se ejecuta una función automatizada para cargar un millón de registros, donde el campo fecha no tiene ningún índice para ver su rendimiento
+DECLARE @count INT = 0;
+
+WHILE @count < 1000000
+BEGIN
+    INSERT INTO Movimiento_Caja (tipo_movimiento, monto, fecha, descripcion, id_turno, id_caja)
+    VALUES (
+        'Ingreso', 
+        RAND() * 1000, 
+        DATEADD(DAY, -ABS(CHECKSUM(NEWID()) % 365), GETDATE()), 
+        'Movimiento generado automáticamente', 
+        1, 
+        1
+    );
+    SET @count = @count + 1;
+END;
+
+-- tiempo de ejecución: 00:02:23
+
+select * from Movimiento_Caja;
+
+-- tiempo de ejecución: 00:00:05
+-- 1.041.984 rows
+
+-- se realiza una búesqueda sin índice en la columna fecha para cierto período
+SET STATISTICS TIME ON;
+SET STATISTICS IO ON;
+
+SELECT * 
+FROM Movimiento_Caja
+WHERE fecha BETWEEN DATEADD(DAY, -300, GETDATE()) AND GETDATE();
+
+SET STATISTICS TIME OFF;
+SET STATISTICS IO OFF;
+-- SQL Server Execution Times:
+-- CPU time = 0 ms,  elapsed time = 0 ms.
+
+-- (856327 rows affected)
+-- Table 'Movimiento_Caja'. Scan count 1, logical reads 11085, physical reads 0, page server reads 0, read-ahead reads 10944, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, 
+-- lob read-ahead reads 0, lob page server read-ahead reads 0.
+
+-- SQL Server Execution Times:
+-- CPU time = 109 ms,  elapsed time = 4178 ms.
+
+-- Completion time: 2024-11-12T20:31:23.7220848-03:00
+
+-- se crea un índice agrupado en la columna fecha y se vuelve a ejecutar la consulta anterior
+CREATE CLUSTERED INDEX IDX_MovimientoCaja_Fecha ON Movimiento_Caja (fecha);
+-- tiempo de ejecución: 00:00:01
+
+
+SELECT * 
+FROM Movimiento_Caja
+WHERE fecha BETWEEN DATEADD(DAY, -300, GETDATE()) AND GETDATE();
+
+-- Se produce el siguiente error: Msg 1902, Level 16, State 3, Line 327
+-- Cannot create more than one clustered index on table 'Movimiento_Caja'. Drop the existing clustered index 'PK_Moviento_Caja_id' before creating another.
+-- Dado que los índices agrupados solo pueden tener un índice por tabla, entra en conflicto con la clave primaria, para ello se elimina dicha clave
+
+ALTER TABLE Movimiento_caja
+DROP CONSTRAINT PK_Moviento_Caja_id
+
+-- se procede a hacer consulta con índice agrupado
+
+-- Respuesta de ejecución: Table 'Movimiento_Caja'. Scan count 1, logical reads 10317, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, 
+-- lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
+-- Completion time: 2024-11-12T19:38:36.0510477-03:00
+
+SET STATISTICS TIME ON;
+SET STATISTICS IO ON;
+
+SELECT * 
+FROM Movimiento_Caja
+WHERE fecha BETWEEN DATEADD(DAY, -300, GETDATE()) AND GETDATE();
+
+SET STATISTICS TIME OFF;
+SET STATISTICS IO OFF;
+
+-- SQL Server Execution Times:
+--  CPU time = 0 ms,  elapsed time = 0 ms.
+
+-- (856327 rows affected)
+-- Table 'Movimiento_Caja'. Scan count 1, logical reads 9139, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, 
+-- lob read-ahead reads 0, lob page server read-ahead reads 0.
+
+-- SQL Server Execution Times:
+--  CPU time = 312 ms,  elapsed time = 3845 ms.
+
+-- Completion time: 2024-11-12T20:32:53.8823400-03:00
+
+
+-- Se elimina el índice creado anteriormente
+
+DROP INDEX IDX_MovimientoCaja_Fecha ON Movimiento_Caja;
+
+DROP INDEX IDX_MovimientoCaja_Fecha_Incluido  ON Movimiento_Caja;
+
+
+-- Definir un índice no agrupado sobre la columna fecha pero que además incluya las columnas seleccionadas y repetir la consulta anterior. Registrar el plan de ejecución utilizado por el motor y los tiempos de respuesta.
+CREATE NONCLUSTERED INDEX IDX_MovimientoCaja_Fecha_Incluido 
+ON Movimiento_Caja (fecha) 
+INCLUDE (tipo_movimiento, monto, descripcion);
+
+SET STATISTICS TIME ON;
+SET STATISTICS IO ON;
+
+
+SELECT * 
+FROM Movimiento_Caja
+WHERE fecha BETWEEN DATEADD(DAY, -300, GETDATE()) AND GETDATE();
+
+SET STATISTICS TIME OFF;
+SET STATISTICS IO OFF;
+
+-- SQL Server Execution Times:
+-- CPU time = 0 ms,  elapsed time = 0 ms.
+
+-- (856327 rows affected)
+-- Table 'Movimiento_Caja'. Scan count 1, logical reads 11085, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, 
+-- lob read-ahead reads 0, lob page server read-ahead reads 0.
+
+-- SQL Server Execution Times:
+-- CPU time = 31 ms,  elapsed time = 3783 ms.
+
+-- Completion time: 2024-11-12T20:37:45.0848858-03:00
+
+-- *** CONCLUSIONES FINALES ***
+-- Consulta sin índice
+-- Tiempo de CPU: 109 ms
+-- Tiempo transcurrido: 4178 ms
+-- Lecturas lógicas: 11,085
+-- Observación: La consulta realiza una exploración completa de la tabla, se puede ver un elevado número de lecturas lógicas debido a la falta de un índice en la columna fecha. Se puede observar en un tiempo de ejecución alto.
+
+-- Consulta con índice agrupado en fecha
+-- Tiempo de CPU: 312 ms
+-- Tiempo transcurrido: 3845 ms
+-- Lecturas lógicas: 9139
+-- Lecturas anticipadas: 0
+-- Observación: La creación de un índice agrupado en fecha reduce considerablemente las lecturas lógicas y mejora el rendimiento de la consulta.
+
+-- Consulta con índice no agrupado en fecha con columnas incluidas (tipo_movimiento, monto, descripcion)
+-- Tiempo de CPU: 31 ms
+-- Tiempo transcurrido: 3783 ms
+-- Lecturas lógicas: 11,085
+-- Lecturas anticipadas: 0
+-- Observación: El índice no agrupado en fecha, incluyendo las columnas tipo_movimiento, monto y descripcion, reduce notablemente el uso de CPU y mejora el tiempo transcurrido en comparación con la consulta sin índices, pero es similar
+-- al tiempo de ejecución de la consulta con índice agrupado. 
+-- Sin embargo, el número de lecturas lógicas vuelve a ser elevado, similar al de la consulta sin índice agrupado.
+
+-- Conclusión
+-- El uso de un índice no agrupado en fecha con columnas incluidas ofrece mejoras en el uso de CPU y reduce el tiempo transcurrido en comparación con la consulta sin índice.
+-- Se puede observar que la consulta con índice agrupado resulta ser la más apropiada en cuanto a tiempo de ejecución y lectural lógicas, ya que si bien la consulta con índice no agrupado es similar en cuanto a tiempo de ejecución
+-- tiene un costo similar en cuanto a lecturas lógicas en comparación con la consulta sin índices.
+
